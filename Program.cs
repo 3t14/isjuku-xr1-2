52bdc70fd1b20e825d5714d89a047ec7e55f9807
3t14 <michiru@r4hs.com>
2016-12-03 07:25:12 +0900

    データ型の確認方法及び変数宣言方法の確認コード

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace xr1_2
 	{
 		public static void Main(string[] args)
 		{
-			
+			// 数値リテラルでもオブジェクト型のメソッドが利用可能
+			Console.WriteLine(123.GetType());
+
+			bool a = true;
+			Console.WriteLine("a = {0}", a.GetType());
+
+			int b = -123;
+			Console.WriteLine("b = {0}", b.GetType());
+
+			double c = 4.56e10;
+			Console.WriteLine("c = {0}", c.GetType());
+
+			string msg = "Hello";
+			Console.WriteLine("msg = {0}", msg.GetType());
+
+			//bool noInit;
+			//// 値が格納されていないのに参照しようとしたため、コンパイルラー
+			//Console.WriteLine("noInit = {0}", noInit.GetType());
+
 		}
 	}
 }
